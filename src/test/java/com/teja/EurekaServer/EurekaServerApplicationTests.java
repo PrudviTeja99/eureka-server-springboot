e8a342576710bee34777f15d4d14fa43ff70047a
Prudviteja <prudvitejabandam987@gmail.com>
2024-10-13 22:07:25 +0530

    init commit

---
+++ src/test/java/com/teja/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teja.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
